de06198b1e922f5aef6095f5e8e6b234201a15da
Belal <mohammadvns121@gmail.com>
2023-09-10 13:28:13 +0530

    Add Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment className="App">
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
